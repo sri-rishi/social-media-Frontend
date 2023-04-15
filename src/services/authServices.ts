4ed676e4ee799f6986f3934aeb3a12043fd06667
sri-rishi <sririshi2612@gmail.com>
2023-04-15 18:54:51 +0530

    feat: created api call function for registering user

--- src/services/authServices.ts
+++ src/services/authServices.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { AuthResponse} from "../pages/auth/authSlice";
 
-export const loginUserService = (username: string, password: string) => {
+const loginUserService = (username: string, password: string) => {
     return axios.post<AuthResponse>(
         "https://social-media-backend.sririshi.repl.co/auth/login",
         {
@@ -10,3 +10,17 @@ export const loginUserService = (username: string, password: string) => {
         }
     )
 }
+
+const registerUserService = (username: string, password: string, firstname: string, lastname: string) => {
+    return axios.post(
+        "https://social-media-backend.sririshi.repl.co/auth/register",
+        {
+            username: username,
+            password: password,
+            firstname: firstname,
+            lastname: lastname
+        }
+    )
+}
+
+export {loginUserService, registerUserService}
