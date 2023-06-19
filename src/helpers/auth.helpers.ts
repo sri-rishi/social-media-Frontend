de1878fec39064585ca3a57e64740cbd5c14eeef
sri-rishi <sririshi2612@gmail.com>
2023-06-19 14:07:03 +0530

    feat: create validating password functionality

---
+++ src/helpers/auth.helpers.ts
@@ -0,0 +1,32 @@
+const validatePassword = (value: string) => {
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return passwordRegex.test(value);
+  };
+
+const checkPasswordCriteria = (value: string) => {
+const criteria = [
+  {
+    label: 'Minimum 8 characters',
+    isValid: value.length >= 8,
+  },
+  {
+    label: 'At least one uppercase letter',
+    isValid: /[A-Z]/.test(value),
+  },
+  {
+    label: 'At least one lowercase letter',
+    isValid: /[a-z]/.test(value),
+  },
+  {
+    label: 'At least one numeric digit',
+    isValid: /\d/.test(value),
+  },
+  {
+    label: 'At least one special character',
+    isValid: /[@$!%*?&]/.test(value),
+  },
+];
+return criteria;
+};
+
+export {validatePassword, checkPasswordCriteria}
