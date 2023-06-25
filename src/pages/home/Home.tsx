83348c582a851cb2108b81ad2a4de2c5a618afb5
sri-rishi <sririshi2612@gmail.com>
2023-06-25 20:37:59 +0530

    feat: added NewPostBox to home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,10 @@
 import { Sidebar } from "../../components"
+import { NewPostBox } from "../../components/newPostBox/NewPostBox"
 
 export const Home: React.FC = () => {
     return(
         <div className="flex flex-row ">
+            <NewPostBox />
             <Sidebar />
             <div>Feed box</div>
             <div>Followbar</div>
