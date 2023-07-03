60ae49e8997944cafadd0064039cfedf98a0aea6
sri-rishi <sririshi2612@gmail.com>
2023-07-03 21:24:41 +0530

    style: changed color

--- src/components/navLinksWithActive/NavLinksWithActive.tsx
+++ src/components/navLinksWithActive/NavLinksWithActive.tsx
@@ -12,7 +12,7 @@ export const NavLinkWithActive: React.FC<NavLinkWithActiveProps> = ({ to, childr
 
     const activeStyle: CSSProperties = {
         background: isActive ? "white" : undefined,
-        color: isActive ? "rgb(56 189 248)" : undefined,
+        color: isActive ? "#45CFDD" : undefined,
         boxShadow: isActive
         ? "0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)"
         : undefined,
