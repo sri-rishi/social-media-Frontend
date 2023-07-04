6e9e42836e3ecfdf89bfa8963fa9ea6816e96f5a
sri-rishi <sririshi2612@gmail.com>
2023-07-04 21:17:24 +0530

    feat: added template and feed component in route

--- src/routes/AllRoutes.tsx
+++ src/routes/AllRoutes.tsx
@@ -1,18 +1,18 @@
-import { Routes, Route, useLocation } from "react-router-dom";
-import { Home, Login, SignUp } from "../features";
-import { useEffect } from "react";
+import { Routes, Route} from "react-router-dom";
+import { Login, SignUp, Template, Feed } from "../features";
+
 
 export const AllRoutes: React.FC = () => {
-  const location = useLocation();
-  const pathname: string = location.pathname;
-
-  useEffect(() => {
-    document.title = pathname === "/" ? "M&T" : pathname.slice(1);
-  }, [pathname]);
-
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route 
+        path="/" 
+        element= {
+          <Template>
+            <Feed />
+          </Template>
+        }
+      />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<SignUp />} />
     </Routes>
