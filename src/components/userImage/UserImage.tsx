5af0942c0a46a42bde5110099bed7cfbf58d2972
sri-rishi <sririshi2612@gmail.com>
2023-07-04 21:14:31 +0530

    feat: create UserImage component

---
+++ src/components/userImage/UserImage.tsx
@@ -0,0 +1,20 @@
+type UserImageType = {
+    imgSrc: string,
+    alt: string,
+    width: string
+}
+
+export const UserImage: React.FC<UserImageType> = ({imgSrc, alt, width}) => {
+    return (
+        <div className={width}>
+            <img 
+                className="w-full rounded-full object-cover" 
+                src={
+                    imgSrc ? 
+                    imgSrc : 
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0Xdf9OyXn9BpWL30gb6cpyLnkiCCbSaH8wVB1007o9WpYBDgb6J1_afDQTdJuqwgE3xM&usqp=CAU"
+                } 
+                alt={alt && alt} />
+        </div>
+    )
+}
