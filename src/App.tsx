a061153852f707b135441afce5c9ce3dcb984e79
sri-rishi <sririshi2612@gmail.com>
2023-07-07 20:57:12 +0530

    feat: read users data from store

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import { AllRoutes } from './routes';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from './app/store';
+import { getUsers } from './features/profilePage/userSlice';
 
 function App() {
+  const {users} = useSelector((store: RootState) => store?.users); 
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    dispatch(getUsers());
+  }, [])
+
   return (
     <div className="App text-slate-600">
       <AllRoutes />
