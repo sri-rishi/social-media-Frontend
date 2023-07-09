23fdd98cc2ba7d8f761a18828630b57d1e4031f3
sri-rishi <sririshi2612@gmail.com>
2023-07-09 20:48:05 +0530

    feat: added api call to backend for follow and unfollow users

--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -1,7 +1,44 @@
-import axios from "axios"
+import axios, { AxiosResponse } from "axios"
 
-const getUsersData = () => {
+const getUsersData = (): Promise<AxiosResponse> => {
     return axios.get("https://social-media-backend.sririshi.repl.co/user")
 }
 
-export { getUsersData }
+
+const followUser = (
+    targetUserId: string,
+    userId: string,
+    token: string
+): Promise<AxiosResponse> => {
+    return axios.post(
+        `https://social-media-backend.sririshi.repl.co/user/${targetUserId}/follow`,
+        {
+            currentUserId: userId,
+        },
+        {
+            headers: {
+                authorization: token
+            }
+        }
+    )
+}
+
+const unFollowUser = (
+    targetUserId: string,
+    userId: string,
+    token: string
+): Promise<AxiosResponse> => {
+    return axios.post(
+        `https://social-media-backend.sririshi.repl.co/user/${targetUserId}/unfollow`,
+        {
+            currentUserId: userId,
+        },
+        {
+            headers: {
+                authorization: token
+            }
+        }
+    )
+}
+
+export { getUsersData, followUser, unFollowUser }
