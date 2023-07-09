e20bf4702bc6adc6afb38b003e6f5a46c76403a6
sri-rishi <sririshi2612@gmail.com>
2023-07-09 20:50:56 +0530

    checked follow/unfollow functionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,21 @@ import './App.css';
 import { AllRoutes } from './routes';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from './app/store';
-import { getUsers } from './features/profilePage/userSlice';
+import { followUnFollowUser, getUsers } from './features/profilePage/userSlice';
 
 function App() {
   const {users} = useSelector((store: RootState) => store?.users); 
   const dispatch = useDispatch<AppDispatch>();
 
+  const followingDetails = {
+    targetUserId:"6490125024420371dd3fe6dc",
+    userId: "64aacc0101ce51a05b88cd7b", 
+    isFollowed: false
+  }
+
   useEffect(() => {
     dispatch(getUsers());
+    dispatch(followUnFollowUser(followingDetails));
   }, [])
 
   return (
