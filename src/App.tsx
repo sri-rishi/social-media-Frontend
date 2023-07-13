f1f9c7f07f6032f8eeb6b1648e9df29fd2d211d9
sri-rishi <sririshi2612@gmail.com>
2023-07-13 20:55:59 +0530

    refactor: removed followUnfollow functionality from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
 import React, {useEffect} from 'react';
 import './App.css';
 import { AllRoutes } from './routes';
-import { useDispatch, useSelector } from 'react-redux';
-import { AppDispatch, RootState } from './app/store';
-import { followUnFollowUser, getUsers } from './features/profilePage/userSlice';
+import { useDispatch} from 'react-redux';
+import { AppDispatch } from './app/store';
+import { getUsers } from './features/profilePage/userSlice';
 
-function App() {
-  const {users} = useSelector((store: RootState) => store?.users); 
+function App() { 
   const dispatch = useDispatch<AppDispatch>();
-
-  const followingDetails = {
-    targetUserId:"6490125024420371dd3fe6dc",
-    userId: "64aacc0101ce51a05b88cd7b", 
-    isFollowed: false
-  }
 
   useEffect(() => {
     dispatch(getUsers());
-    dispatch(followUnFollowUser(followingDetails));
   }, [])
 
   return (
