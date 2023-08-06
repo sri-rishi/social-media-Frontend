8acb63daae7132078d38bed7ab48130a1eb44998
sri-rishi <sririshi2612@gmail.com>
2023-08-06 20:23:51 +0530

    feat: dispatch call for getAllPost function

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import { AllRoutes } from './routes';
 import { useDispatch} from 'react-redux';
 import { AppDispatch } from './app/store';
 import { getUsers } from './features/profilePage/userSlice';
+import { getAllPosts } from './components/post/postSlice';
 
 function App() { 
   const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
     dispatch(getUsers());
+    dispatch(getAllPosts());
   }, [])
 
   return (
