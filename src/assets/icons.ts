13be649536133e586ad737bf870a973d41c8b311
sri-rishi <sririshi2612@gmail.com>
2023-08-06 20:11:01 +0530

    style: imported icons

--- src/assets/icons.ts
+++ src/assets/icons.ts
@@ -1,23 +1,31 @@
-import {AiOutlineEyeInvisible, AiOutlineEye} from "react-icons/ai";
-import {GoHome} from "react-icons/go";
-import {MdOutlineExplore} from "react-icons/md";
-import {IoBookmarkOutline} from "react-icons/io5";
-import { IoMdSearch} from "react-icons/io";
-import {CgProfile} from "react-icons/cg";
-import {GiFeather} from "react-icons/gi"
-import {FiLogOut} from "react-icons/fi";
-import {FaTimes} from "react-icons/fa";
+import { AiOutlineEyeInvisible, AiOutlineEye, AiFillHeart } from "react-icons/ai";
+import { GoHome } from "react-icons/go";
+import { MdOutlineExplore } from "react-icons/md";
+import { IoBookmarkOutline, IoBookmark } from "react-icons/io5";
+import { IoMdSearch } from "react-icons/io";
+import { CgProfile } from "react-icons/cg";
+import { GiFeather } from "react-icons/gi"
+import { FiLogOut } from "react-icons/fi";
+import { FaTimes } from "react-icons/fa";
+import { RiHeart3Line } from "react-icons/ri";
+import { BsThreeDots } from "react-icons/bs";
+import { TbMessageCircle2 } from "react-icons/tb"
 
 
 export {
     AiOutlineEyeInvisible,
     AiOutlineEye,
-    GoHome, 
-    MdOutlineExplore, 
-    IoBookmarkOutline, 
-    CgProfile, 
-    GiFeather, 
-    IoMdSearch, 
+    GoHome,
+    MdOutlineExplore,
+    IoBookmarkOutline,
+    CgProfile,
+    GiFeather,
+    IoMdSearch,
     FiLogOut,
-    FaTimes
+    FaTimes,
+    AiFillHeart,
+    IoBookmark,
+    RiHeart3Line,
+    BsThreeDots,
+    TbMessageCircle2
 }
--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,14 +1,35 @@
-import { AiOutlineEye, AiOutlineEyeInvisible, GoHome, MdOutlineExplore, IoBookmarkOutline, CgProfile, GiFeather, IoMdSearch, FiLogOut, FaTimes } from "./icons";
+import {
+    AiOutlineEye,
+    AiOutlineEyeInvisible,
+    GoHome,
+    MdOutlineExplore,
+    IoBookmarkOutline,
+    CgProfile,
+    GiFeather,
+    IoMdSearch,
+    FiLogOut,
+    FaTimes,
+    AiFillHeart,
+    IoBookmark,
+    RiHeart3Line,
+    BsThreeDots,
+    TbMessageCircle2
+} from "./icons";
 
 export {
-    AiOutlineEyeInvisible , 
+    AiOutlineEyeInvisible,
     AiOutlineEye,
-    GoHome, 
-    MdOutlineExplore, 
-    IoBookmarkOutline, 
-    CgProfile, 
-    GiFeather, 
-    IoMdSearch, 
-    FiLogOut, 
-    FaTimes
+    GoHome,
+    MdOutlineExplore,
+    IoBookmarkOutline,
+    CgProfile,
+    GiFeather,
+    IoMdSearch,
+    FiLogOut,
+    FaTimes,
+    AiFillHeart,
+    IoBookmark,
+    RiHeart3Line,
+    BsThreeDots,
+    TbMessageCircle2
 }
